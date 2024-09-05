9542d286c99f4e718ed6fb52a236ec29be5d2806
Krish Chopra <krish.chopra23@gmail.com>
2024-09-05 02:42:27 -0400

    edit description

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,12 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-      Hey there! I&apos;m Krish — a rising second year Computer Science & Business Administration student at the University of Waterloo, passionate about emerging technologies and entrepreneurship. I love developing innovative, scalable solutions to real-world problems through pitch/case competitions, hackathons, and projects. Let&apos;s connect! Reach out anytime at: krish.chopra@uwaterloo.ca.
+        Hey there! I&apos;m Krish — a second-year Computer Science & Business
+        Administration student at the University of Waterloo, passionate about
+        emerging technologies and entrepreneurship. I love developing
+        innovative, scalable solutions to real-world problems through pitch/case
+        competitions, hackathons, and projects. Let&apos;s connect! Reach out
+        anytime at: krish.chopra@uwaterloo.ca.
       </div>
     </main>
   );
